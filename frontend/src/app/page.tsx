7a92532815d39ca458e791ae370a3c89f9e88c47
imt-eiaz <imteiaz@gmail.com>
2026-04-26 12:54:55 +0100

    CSS applied - Title Changed to two lines

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -17,12 +17,15 @@ export default function Home() {
   return (
     <div className="min-h-screen bg-gradient-to-br from-blue-600 to-blue-800 flex items-center justify-center px-4">
       <div className="text-center">
-        <h1 className="text-5xl font-bold text-white mb-4">
-          Candle Public School Management System
+        <h1 className="text-8xl font-bold text-white mb-4">
+          Candle Public School
         </h1>
-        <p className="text-xl text-blue-100 mb-8">
+        <h3 className="text-4xl font-bold text-white mb-8">
+          Management System
+        </h3>
+        {/* <p className="text-2xl text-blue-100 mb-8">
           Comprehensive solution for managing academic institutions
-        </p>
+        </p> */}
         <div className="space-x-4">
           <Link
             href="/auth/login"
